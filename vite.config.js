77aae8630d566bae42f86d13635fd913b0f0dc2d
reynaldlamury <51454984+reynaldlamury@users.noreply.github.com>
2023-03-11 14:16:45 +0700

    initial init

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import sass from "sass";
+
+export default defineConfig({
+  // base: '/valentine2023',
+  root: "src",
+  build: {
+    outDir: "../dist",
+  },
+
+  assets: {
+		include: ["**/*.mp3", "**/*.jpg", "**/*.JPG", "**/*.png", "**/*.PNG", "**/*svg"],
+  },
+
+  plugins: [
+    {
+      name: "sass",
+      renderChunk: (code) => sass.renderSync({ data: code }).css.toString(),
+    },
+  ],
+});
